3ab85f7b8ef079749754aec2044c6322cf9cce7e
Chris Erickson <chriserickson@live.com>
2025-02-26 18:51:16 -0800

    Update loadComponents.js

--- assets/js/loadComponents.js
+++ assets/js/loadComponents.js
@@ -9,6 +9,7 @@ window.onload = function () {
         console.log("Fetched components successfully");
 
         insertComponents(headerData, sidebarData);
+        loadMainJS();
     })
     .catch(error => console.error("Error loading components:", error));
 };
@@ -20,4 +21,11 @@ function insertComponents(headerData, sidebarData, navbarData) {
     if (document.querySelector("#sidebar")) {
         document.querySelector("#sidebar").innerHTML = sidebarData;
     }
+}
+function loadMainJS() {
+    console.log("Loading main.js...");
+    let script = document.createElement("script");
+    script.src = "/assets/js/main.js";
+    script.defer = true;
+    document.body.appendChild(script);
 }
